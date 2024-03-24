6fb93689cb82c4ffebe145ea73467cdbc11f81c0
piyush <piyush@destreet.co>
2024-03-25 02:47:06 +0530

    removed db utils; added dev url from railway app

--- dist/assets/index-e3118428.js
+++ dist/assets/index-e3118428.js
@@ -1,4 +1,4 @@
-import{b as _r,d as Ve,r as le,ax as Di,au as ye,aC as H4}from"./antd-e79f131b.js";import{p as Ar,q as $4,P as ue,j as q}from"./index-ef0f63bd.js";const M0=["http","https","mailto","tel"];function G4(e){const t=(e||"").trim(),n=t.charAt(0);if(n==="#"||n==="/")return t;const r=t.indexOf(":");if(r===-1)return t;let a=-1;for(;++a<M0.length;){const i=M0[a];if(r===i.length&&t.slice(0,i.length).toLowerCase()===i)return t}return a=t.indexOf("?"),a!==-1&&r>a||(a=t.indexOf("#"),a!==-1&&r>a)?t:"javascript:void(0)"}/*!
+import{b as _r,d as Ve,r as le,ax as Di,au as ye,aC as H4}from"./antd-e79f131b.js";import{p as Ar,q as $4,P as ue,j as q}from"./index-a740093a.js";const M0=["http","https","mailto","tel"];function G4(e){const t=(e||"").trim(),n=t.charAt(0);if(n==="#"||n==="/")return t;const r=t.indexOf(":");if(r===-1)return t;let a=-1;for(;++a<M0.length;){const i=M0[a];if(r===i.length&&t.slice(0,i.length).toLowerCase()===i)return t}return a=t.indexOf("?"),a!==-1&&r>a||(a=t.indexOf("#"),a!==-1&&r>a)?t:"javascript:void(0)"}/*!
  * Determine if an object is a Buffer
  *
  * @author   Feross Aboukhadijeh <https://feross.org>
--- src/providers/authFirebase.ts
+++ src/providers/authFirebase.ts
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { AuthProvider } from "@refinedev/core";
 
 import { createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from "firebase/auth";
